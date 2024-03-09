8d603f1118100cdf414a478633d945bff00d7d0b
Fabian Flores <famercado24@gmail.com>
2024-03-09 17:25:16 -0400

    creacion de la clase proceso

---
+++ process.cpp
@@ -0,0 +1,9 @@
+#include "process.h"
+
+Process::Process(/* args */)
+{
+}
+
+Process::~Process()
+{
+}
---
+++ process.h
@@ -0,0 +1,9 @@
+class Process
+{
+private:
+    unsigned pid;
+    unsigned burst_time;
+public:
+    Process(/* args */);
+    ~Process();
+};
