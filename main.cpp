a7fb9c44a79dfa34982906b6052fb955de27c843
danieldm <60081872+danieldm352@users.noreply.github.com>
2024-03-17 14:03:16 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,17 @@
 #include "process.h"
 #include "fcfs.h"
 #include "SJF.h"
+#include "random_selection.h"
 
 int main()
 {
     //std::cout << "hola";
     //Fcfs fcfs(8); 
     //fcfs.execute();
-    SJF sjf(7);
-    sjf.execute();
+    //SJF sjf(7);
+    //sjf.execute();
+    RandomSelection randomselection(7);
+    randomselection.execute();
     
     return 0;
 }
