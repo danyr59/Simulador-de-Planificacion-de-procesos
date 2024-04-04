68b1526d437f36e11f0fdf096e31881867a1f0bd
Daniel Rangel <danyelrange59@gmail.com>
2024-04-03 21:48:22 -0400

    probabilidad cola de procesos

--- process.cpp
+++ process.cpp
@@ -36,7 +36,7 @@ void Process::generate_block_point()
     if(burst_time > 2)
     {
         
-        int probability = generateRandomNum(0, 20);
+        int probability = generateRandomNum(0, 3);
         if (probability == 1)
         {
             //this->block_point = 4;
